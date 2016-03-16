7be50c4d250b289b0dbddef0974243934a1b1fe0
anna hunter <annfmonty@gmail.com>
2016-03-16 08:00:47 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+
+
+var tabId = document.querySelector(".tbbutton");
+tabId.addEventListener("click", showTab);
+var showTab = function() {
+        if ( class == "red") {
+          .red.rdata = 'selected';
+          contentDivs[id].className = 'tabContent';
+        } else if
+        {
+          .blue.bdata = 'selected';
+          contentDivs[id].className = 'tabContent hide';
+        }
+      }
+
+      // Stop the browser following the link
+      return false;
+    }
